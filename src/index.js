601ce8263e0b40de6fccfd454a51dba505e3d14c
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 17:38:32 +0300

    refactor: done with recursive algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-    return parseInt(`${Math.abs(n)}`.split('').reverse().join(''));
+module.exports = function reverse(n) {
+    return reversePositiveInt(Math.abs(n), 0);
+}
+
+function reversePositiveInt(n, acc) {
+    if (n == 0) return acc;
+    return reversePositiveInt(Math.floor(n/10), acc*10 + n%10);
 }
